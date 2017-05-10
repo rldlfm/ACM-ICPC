28a4b5da275e00d64d8eb8306e3649b11a229323
rldlfm <rldlfm@qq.com>
2017-05-10 16:50:42 +0800

    Revert "Revert "2015/5/18""
    This reverts commit 163b86123cc9e86976557f275b6b1e2961b1c260.

---
+++ HDU/1000.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a,b;
+		while ( scanner.hasNextInt() ) {
+			a = scanner.nextInt();
+			b = scanner.nextInt();
+			System.out.println(a + b);
+		}
+		scanner.close();
+	}
+}
+
+//Accept
